dafca6b21194d728c861b530ad8e6a9a7b665297
siddhant ota <tothesip@gmail.com>
2023-04-03 16:42:14 +0200

    added flag feature

--- file_handling/gigacat.c
+++ file_handling/gigacat.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
-
-	if (argc == 2)
+	if (strcmp(argv[1], "-r") == 0)
 	{
+		printf("the flag given was: %s\n", argv[1]);
 		char content[1000];
-		FILE *stream = fopen(argv[1], "r");
+		FILE *stream = fopen(argv[2], "r");
 
 		if (!stream)
 		{
 			printf("error opening file\n");
-		} else {
+		}
+		else
+		{
 
-			printf("opened the %s file successfully \n", argv[1]);
+			printf("opened the %s file successfully \n", argv[2]);
 			printf("__________________________________________________\n");
 			printf("\n");
 
@@ -21,12 +24,11 @@ int main(int argc, char *argv[])
 			{
 				printf("%s", content);
 			}
-			
 		}
-	}
-	else
-	{
-		printf("please give the file to read as arguement to gigacat.\n");
+
+		/* code */
+	} else{
+		printf("if failed \n");
 	}
 	return 0;
 }
